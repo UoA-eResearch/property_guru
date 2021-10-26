eda1bb25c636df008b82de24d9800553743f482e
Nick Young <nick.young@auckland.ac.nz>
2021-10-27 12:48:14 +1300

    pagination

--- property_guru/spiders/propertyguru.py
+++ property_guru/spiders/propertyguru.py
@@ -4,6 +4,7 @@ import json
 import logging
 from pprint import pprint
 import pandas as pd
+from tqdm import tqdm
 
 logger = logging.getLogger("propertyguru")
 
@@ -27,24 +28,25 @@ class PropertyguruSpider(scrapy.Spider):
                 "rememberPassword": "on",
             },
         )
-        yield FormRequest(
-            self.render_url,
-            formdata={
-                "action": "MWUpdateQuery",
-                "mwDisplayOutput": "MWTable",
-                "region_id": "2",  # Auckland Region
-                "listed_in": "1960-01-01|2021-10-26",  # All time
-                "district_id": "76",  # Auckland
-                "listing_status": "2",  # options = all, 1 (Active), 2 (Withdrawn)
-                "suburb_id": "2728",  # Auckland Central
-                "listing_type_id": "13",  # Commercial Lease
-                "offset": "30020",
-                "hid": "2",
-                "hash": "F7A5D3D1-3C65-22B7-3703-45BACD968874",
-                "fastSearch": "",
-            },
-            callback=self.handle_page,
-        )
+        for offset in tqdm(range(30040, 0, -20)):
+            yield FormRequest(
+                self.render_url,
+                formdata={
+                    "action": "MWUpdateQuery",
+                    "mwDisplayOutput": "MWTable",
+                    "region_id": "2",  # Auckland Region
+                    "listed_in": "1960-01-01|2021-10-26",  # All time
+                    "district_id": "76",  # Auckland
+                    "listing_status": "2",  # options = all, 1 (Active), 2 (Withdrawn)
+                    "suburb_id": "2728",  # Auckland Central
+                    "listing_type_id": "13",  # Commercial Lease
+                    "offset": str(offset),
+                    "hid": "2",
+                    "hash": "F7A5D3D1-3C65-22B7-3703-45BACD968874",
+                    "fastSearch": "",
+                },
+                callback=self.handle_page,
+            )
 
     def parse(self, response):
         pass
